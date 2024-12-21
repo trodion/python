ac96fa88b4cd6a437828b5c74a9c47f706c6ce2d
trodion <rodion.toporoff@yandex.ru>
2024-12-21 20:50:44 +0300

    Initial commit. Add fibonacci func and tests

---
+++ test.py
@@ -0,0 +1,11 @@
+from main import fibonacci
+
+
+def test_fibonacci():
+    assert fibonacci(1) == 0, "Test case 1 failed"
+    assert fibonacci(2) == 1, "Test case 2 failed"
+    assert fibonacci(5) == 3, "Test case 3 failed"
+    assert fibonacci(10) == 34, "Test case 4 failed"
+
+
+test_fibonacci()
