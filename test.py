0c7b960e2599133fab58d3f84ea52d683726a44b
trodion <rodion.toporoff@yandex.ru>
2024-12-21 20:51:42 +0300

    Failed test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from main import fibonacci
 def test_fibonacci():
     assert fibonacci(1) == 0, "Test case 1 failed"
     assert fibonacci(2) == 1, "Test case 2 failed"
-    assert fibonacci(5) == 3, "Test case 3 failed"
+    assert fibonacci(5) == 5, "Test case 3 failed"
     assert fibonacci(10) == 34, "Test case 4 failed"
 
 
